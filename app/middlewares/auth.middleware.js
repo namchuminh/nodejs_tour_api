39da14eb3f0fb84da06cc33db9f98d903f394dde
Nam Chu Minh <chuminhnamma@gmail.com>
2023-11-02 02:01:28 +0700

    fix auth.middelware

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -2,19 +2,23 @@ const jwt = require("jsonwebtoken");
 const Blacklist = require("../models/blacklist.model.js");
 
 const auth = async (req, res, next) => {
+    if(!req.header("Authorization")){
+        return res.status(401).json({ error: "Token không tồn tại!" });
+    }
     try {
         const token = req.header("Authorization").split(" ")[1];
-        if (!token) return res.status(401).json({ error: "Token không tồn tại" });
-
-        const blacklistedToken = await Blacklist.findOne({ where: { token: token } });
+        if (!token) {
+            return res.status(401).json({ error: "Token không tồn tại" });
+        }
         const decoded = jwt.verify(token, "SECRET_KEY");
 
-        if(!decoded) return res.status(401).json({ message: "Token đã hết hạn hoặc không hợp lệ!" });
-        
+        const blacklistedToken = await Blacklist.findOne({ where: { token: token } });
+
         if (blacklistedToken) {
             return res.status(401).json({ message: "Token đã hết hạn hoặc không hợp lệ!" });
-        }
+        }    
 
+        console.log(decoded)
         req.user = decoded;
         next();
     } catch (err) {
